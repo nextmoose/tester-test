dadaf129c36b44d468f8121d92a7f8a0dab69354
Emory Merryman <emory.merryman@gmail.com>
2023-01-30 22:16:02 -0500

    a better happy

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
   {
-    inputs = { flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ; nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f583684" ;} ;
+    inputs = { flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ; nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843" ;} ;
     outputs =
       { flake-utils , nixpkgs , self } :
         flake-utils.lib.eachDefaultSystem
