c71e6cd4d018d3ea2840dc6035b1831604706e2d
Emory Merryman <emory.merryman@gmail.com>
2023-01-30 22:09:53 -0500

    a better happy

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
   {
-    inputs = { flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ; } ;
+    inputs = { flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ; nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f583684" ;} ;
     outputs =
-      { flake-utils , self } :
+      { flake-utils , nixpkgs , self } :
         flake-utils.lib.eachDefaultSystem
 	  (
 	    system :
@@ -17,9 +17,12 @@
 		      } ;
 		      test = implementation ;
 		    in
-		      {
-		        happy = tester : tester "6be2b381-e0e6-4041-9de1-4627c573874d" ( testee : builtins.concatStringsSep "" ( builtins.attrNames ( testee implementation test ) ) ) true "devShell" ;
-		      } ;
+		      let
+		        pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
+			in
+		          {
+		            happy = tester : tester "6be2b381-e0e6-4041-9de1-4627c573874d" ( testee : testee implementation test ) true ( pkgs.mkShell { } ) ;
+		          } ;
 	      }
 	  ) ;
   }
