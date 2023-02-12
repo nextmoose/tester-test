690c6fc91d0afe0a69b0647deaed01be6fc0c635
Emory Merryman <emory.merryman@gmail.com>
2023-02-12 15:19:35 -0500

    move lambda into set

--- flake.nix
+++ flake.nix
@@ -14,10 +14,10 @@
                   let
                     pkgs = builtins.getAttr system nixpkgs.defaultPackages ;
                     in
-                      {
-                        happy = lambda : lambda ( implementation : implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
-                        sad = lambda : lambda ( implementation : implementation null ) false false ;
-                      } ;
+		      {
+		        happy = tester : tester ( implementation : implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+		        sad = tester : tester ( implementation : implementation null ) false false ;
+		      } ;
               }
           ) ;
   }
