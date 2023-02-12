063e4d0a9625cf4c27ee1e88c175778d19e73e9a
Emory Merryman <emory.merryman@gmail.com>
2023-02-11 19:43:49 -0500

    move lambda into set

--- flake.nix
+++ flake.nix
@@ -2,24 +2,22 @@
     inputs =
       {
         flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
-	nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843" ;
+        nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843" ;
       } ;
     outputs =
       { flake-utils , self , nixpkgs } :
         flake-utils.lib.eachDefaultSystem
           (
-	    system :
-	      {
-	        lib =
-		  lambda :
-		    let
-		      pkgs = builtins.getAttr system nixpkgs.defaultPackages ;
-		      in
-		        {
-		          happy = lambda ( implementation : implementation [ ] ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
-  		          sad = lambda ( implementation : implementation null ) null null ;
-			}
-		    }
-	      }
+            system :
+              {
+                lib =
+                  let
+                    pkgs = builtins.getAttr system nixpkgs.defaultPackages ;
+                    in
+                      {
+                        happy = lambda : lambda ( implementation : implementation [ ] ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+                        sad = lambda : lambda ( implementation : implementation null ) null null ;
+                      } ;
+              }
           ) ;
   }
