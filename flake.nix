7db61783abe74d0c787812dbbe3b19a399cbe168
Emory Merryman <emory.merryman@gmail.com>
2023-02-12 20:17:29 -0500

    sad float

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
                       {
                         happy = tester : tester ( implementation : check implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
                         sad = tester : tester ( implementation : check implementation null ) false false ;
+                        float = tester : tester ( implementation : check implementation 0.0 ) false false ;
                       } ;
               }
           ) ;
