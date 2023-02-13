b3ad123c5eb36861f1d761413cb87b7709b7c702
Emory Merryman <emory.merryman@gmail.com>
2023-02-12 21:03:17 -0500

    happy list

--- flake.nix
+++ flake.nix
@@ -27,7 +27,11 @@
                     pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
                     in
                       {
-                        happy = tester : tester ( implementation : check implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+                        happy =
+			  {
+			    list = tester : tester ( implementation : check implementation [ ] ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+			    set = tester : tester ( implementation : check implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+			  }
                         sad =
 			  {
 			    bool = tester : tester ( implementation : check implementation true ) false false ;
