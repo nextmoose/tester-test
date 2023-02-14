aa7a96fc527668fd8400f1a98b4eb8b1adbc8b4c
Emory Merryman <emory.merryman@gmail.com>
2023-02-13 21:04:36 -0500

    incorrect path

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
                                 problem =
                                   {
                                     lambda = tester : tester ( implementation : check implementation { lambda = bad ; } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "${ pkgs.coreutils }/bin/echo lambda" ) ] ; } ; } ;
+                                    set = tester : tester ( implementation : check implementation { { alpha = { lambda = bad ; } ; } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "${ pkgs.coreutils }/bin/echo alphalambda" ) ] ; } ; } ;
                                   } ;
                               } ;
                         sad =
