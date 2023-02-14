4d949a454d590de1881abeb5fa472c1fdaddb552
Emory Merryman <emory.merryman@gmail.com>
2023-02-13 19:17:54 -0500

    happy lambda case

--- flake.nix
+++ flake.nix
@@ -27,7 +27,22 @@
                     pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
                     in
                       {
-                        happy = tester : tester ( implementation : check implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+                        happy =
+			  {
+			    simple =
+			      {
+			        lambda =
+				  lambda :
+			            ( tester : tester ( implementation : check implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } )
+				    true
+				    { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+			        list = tester : tester ( implementation : check implementation [ ] ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+			        set = tester : tester ( implementation : check implementation { } ) true { devShell = pkgs.mkShell { buildInputs = [ ( pkgs.writeShellScriptBin "check" "" ) ] ; } ; } ;
+			      } ;
+			    complex =
+			      {
+			      } ;
+			  } ;
                         sad =
 			  {
 			    bool = tester : tester ( implementation : check implementation true ) false false ;
